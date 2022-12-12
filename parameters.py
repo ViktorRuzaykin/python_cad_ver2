cd364366f51073f55cf12bf0897228934369ea41
ruzaikin <ruzaikinviktor@gmail.com>
2022-12-12 17:31:03 +0800

    add2

--- parameters.py
+++ parameters.py
@@ -8,48 +8,48 @@ def read_parameters():
 
 
 param = read_parameters()
-print(param)
-SCALE_WIDTH = 1 / 7  # соотношение ширины шкалы, ширина к высоте
-CONDITIONAL_HORIZON = 5  # значение для определения условного горизонта
-OFFSET_TEXT_SCALE = 4  # отступ текста от шкалы профиля
-ROTATION_ANGLE_90 = 1.57079632679  # угол 90 градусов в радианах
-OFFSET_TEXT_LINE = -1.1  # отступ текста от линии в подвале
-OFFSET_PROFILE = 8.5  # отступ основного профиля от шапки подвала
-HEIGHT_TEXT = 4
-OFFSET_NEW_PROFILE = 40
-WIDTH_BASEMENT = 85.4
-VARIATION_DITCH = (-5, 5)  # допуск для траншеи
-VARIATION_PILLOW = (-5, 5)  # допуск для подушки
-VARIATION_PIPE = (-15, 15)  # допуск для трубы
-VARIATION_FILLING = (1, 5)  # допуск для присыпки
-HEIGHT_PILLOW = 0.10  # высота подушки
-DIAMETER_PIPE = 0.110  # диаметр трубы
-HEIGHT_FILLING = 0.30  # высота присыпки
-TOP_PIPE = False
+
+SCALE_WIDTH = param['scale_width']  # соотношение ширины шкалы, ширина к высоте
+CONDITIONAL_HORIZON = param['conditional_horizon']  # значение для определения условного горизонта
+OFFSET_TEXT_SCALE = param['offset_text_scale']  # отступ текста от шкалы профиля
+ROTATION_ANGLE_90 = param['rotation_angle_90']  # угол 90 градусов в радианах
+OFFSET_TEXT_LINE = param['offset_text_line']  # отступ текста от линии в подвале
+OFFSET_PROFILE = param['offset_profile']  # отступ основного профиля от шапки подвала
+HEIGHT_TEXT = param['height_text']
+OFFSET_NEW_PROFILE = param['offset_new_profile']
+WIDTH_BASEMENT = param['width_basement']
+VARIATION_DITCH = param['variation_ditch']  # допуск для траншеи
+VARIATION_PILLOW = param['variation_pillow']  # допуск для подушки
+VARIATION_PIPE = param['variation_pipe']  # допуск для трубы
+VARIATION_FILLING = param['variation_filling']  # допуск для присыпки
+HEIGHT_PILLOW = param['height_pillow']  # высота подушки
+DIAMETER_PIPE = param['diameter_pipe']  # диаметр трубы
+HEIGHT_FILLING = param['height_filling']  # высота присыпки
+TOP_PIPE = param['top_pipe']
 NAME_COLUMN = ('пк', 'плюс', 'H трубы', 'Н земли', 'Н транш', 'Н ф.транш', 'Н подуш', 'Н ф.подуш', 'Н ф.трубы',
                'Н обсып', 'Н ф.обсып')
 
 PATH_FILE = {
-    'ditch': 'ditch.txt',
-    'pillow': 'pillow.txt',
-    'pipe': 'pipe.txt',
-    'filling': 'filling.txt',
+    'ditch': param['path_file']['ditch'],
+    'pillow': param['path_file']['pillow'],
+    'pipe': param['path_file']['pipe'],
+    'filling': param['path_file']['filling'],
 }
 
-STEP_HORIZONTAL_DITCH = [14, 14, 20, 20, 20, 28, 20]  # шаг горизонтальных линий подвала разработки
-_step_hor = [14, 20, 20, 28, 20]
+STEP_HORIZONTAL_DITCH = param['step_horizontal_ditch']  # шаг горизонтальных линий подвала разработки
+_step_hor = param['_step_hor']
 STEP_HORIZONTAL_PILLOW = _step_hor  # шаг горизонтальных линий подвала подушки
 STEP_HORIZONTAL_PIPE = _step_hor  # шаг горизонтальных линий подвала укладки
 STEP_HORIZONTAL_FILLING = _step_hor  # шаг горизонтальных линий подвала обсыпки
 
-DECIMAL_PLACES = 3
+DECIMAL_PLACES = param['decimal_places']
 
-LEVEL_DITCH_PROJECT = 126.00
-LEVEL_DITCH_PK = 102.00
-LEVEL_DITCH_ACTUAL = 58.00
-LEVEL_DITCH_EARTH = 38.00
-LEVEL_DITCH_DIFF = 20.00
-LEVEL_DITCH_DEPTH = 7.00
+LEVEL_DITCH_PROJECT = param['level_ditch_project']
+LEVEL_DITCH_PK = param['level_ditch_pk']
+LEVEL_DITCH_ACTUAL = param['level_ditch_actual']
+LEVEL_DITCH_EARTH = param['level_ditch_earth']
+LEVEL_DITCH_DIFF = param['level_ditch_diff']
+LEVEL_DITCH_DEPTH = param['level_ditch_depth']
 
 L1 = 92.00
 L2 = 68.00
@@ -72,6 +72,3 @@ DIFFERENCE_TYPE = {
     'см': 100,
     'мм': 1000,
 }
-
-
-
